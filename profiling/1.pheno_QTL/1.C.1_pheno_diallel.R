d3628f328f7156f85d2cc4b02c5d1794f9715193
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-12-02 15:41:43 -0800

    updated phonetype part.

--- profiling/1.pheno_QTL/1.C.1_pheno_diallel.R
+++ profiling/1.pheno_QTL/1.C.1_pheno_diallel.R
@@ -3,36 +3,27 @@
 ## plot the phenotypic traits
 
 
-readPheno <- function(pwd="~/Documents/Heterosis_GWAS/HGWAS_proj/data/",
-  infile="akw_v2_GenSel_fullset.txt", outfile="akw_v2_GenSel_diallel.txt"){
+readPheno <- function(infile="akw_v2_GenSel_fullset.txt", outfile="akw_v2_GenSel_diallel.txt"){
   
-  setwd(pwd)
+  #setwd(pwd)
   pheno <- read.table(infile, header=TRUE)
   
   dpheno <- subset(pheno, pheno[,4] == "Diallel")
   message(sprintf("[ %s ] genotypes", nrow(dpheno)))
   ###output
-  write.table(dpheno[, 1:3], outfile,
-              sep="\t", row.names=FALSE, quote=FALSE)
+  write.table(dpheno[, 1:3], outfile, sep="\t", row.names=FALSE, quote=FALSE)
   
   return(dpheno[, 1:3])
 }
 
 
 ######
-krn <- readPheno(pwd="~/Documents/Heterosis_GWAS/HGWAS_proj/data/",
-                 infile="krn_v2_GenSel_fullset.txt", outfile="krn_v2_GenSel_diallel.txt")
-cd <- readPheno(pwd="~/Documents/Heterosis_GWAS/HGWAS_proj/data/",
-                 infile="cd_v2_GenSel_fullset.txt", outfile="cd_v2_GenSel_diallel.txt")
-cl <- readPheno(pwd="~/Documents/Heterosis_GWAS/HGWAS_proj/data/",
-                 infile="cl_v2_GenSel_fullset.txt", outfile="cl_v2_GenSel_diallel.txt")
-cw <- readPheno(pwd="~/Documents/Heterosis_GWAS/HGWAS_proj/data/",
-                 infile="cw_v2_GenSel_fullset.txt", outfile="cw_v2_GenSel_diallel.txt")
-akw <- readPheno(pwd="~/Documents/Heterosis_GWAS/HGWAS_proj/data/",
-                 infile="akw_v2_GenSel_fullset.txt", outfile="akw_v2_GenSel_diallel.txt")
-tkw <- readPheno(pwd="~/Documents/Heterosis_GWAS/HGWAS_proj/data/",
-                 infile="tkw_v2_GenSel_fullset.txt", outfile="tkw_v2_GenSel_diallel.txt")
-kc <- readPheno(pwd="~/Documents/Heterosis_GWAS/HGWAS_proj/data/",
-                 infile="kc_v2_GenSel_fullset.txt", outfile="kc_v2_GenSel_diallel.txt")
+krn <- readPheno(infile="data/krn_v2_GenSel_fullset.txt", outfile="data/krn_v2_GenSel_diallel.txt")
+cd <- readPheno(infile="data/cd_v2_GenSel_fullset.txt", outfile="data/cd_v2_GenSel_diallel.txt")
+cl <- readPheno(infile="data/cl_v2_GenSel_fullset.txt", outfile="data/cl_v2_GenSel_diallel.txt")
+cw <- readPheno(infile="data/cw_v2_GenSel_fullset.txt", outfile="data/cw_v2_GenSel_diallel.txt")
+akw <- readPheno(infile="data/akw_v2_GenSel_fullset.txt", outfile="data/akw_v2_GenSel_diallel.txt")
+tkw <- readPheno(infile="data/tkw_v2_GenSel_fullset.txt", outfile="data/tkw_v2_GenSel_diallel.txt")
+kc <- readPheno(infile="data/kc_v2_GenSel_fullset.txt", outfile="data/kc_v2_GenSel_diallel.txt")
 
 
